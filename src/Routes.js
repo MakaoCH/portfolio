fdd0f73aea061af1c0c13094ae3cdcbc45b26624
Mickael Brothier <mickaelbrothier@outlook.com>
2023-07-04 16:12:15 +0200

    màj routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ProjectDescription from "../src/pages/ProjectDescription/ProjectDescritpion";
 import Layout from './components/Layout/Layout';
@@ -7,19 +7,17 @@ import Layout from './components/Layout/Layout';
 function App() {
   return (
     <div>
-        <BrowserRouter>
-        
-          <Routes >
-          <Route element={<Layout/>}>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/ProjectDescription/:id' element={<ProjectDescription/>}/>
-            
-            </Route>
-          </Routes>
-         
-        </BrowserRouter>
+      <BrowserRouter basename="/portfolio-mickael-brothier">
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/ProjectDescription/:id' element={<ProjectDescription />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
